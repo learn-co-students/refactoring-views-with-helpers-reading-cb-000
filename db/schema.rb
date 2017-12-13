d679e9d842d0206a414cf420d893e3655eaed683
Matt Hough <matt@hough.tv>
2017-12-14 09:02:16 +1000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113011820) do
+ActiveRecord::Schema.define(version: 20171213223504) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20160113011820) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
+    t.text     "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
