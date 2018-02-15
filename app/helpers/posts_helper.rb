d6d19944770daf8c3a18d8a5a05967f949efabb4
Heather Christman <efl7a@yahoo.de>
2018-02-15 12:35:37 -0500

    Done.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def last_updated(post)
+    post.updated_at.present? ? post.updated_at.strftime("last updated %A, %b %e, at %l:%M %p"):nil
+  end
 end
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe AuthorsController, type: :controller do
 
   describe "GET #show" do
+    before do
+      @author = Author.create(name: "Fred Flintstone")
+    end
+
     it "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
